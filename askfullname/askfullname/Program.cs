d79322b5483560f8484887f0dfbfb720e6c4c1cd
Linus Alpsten <linus.alpsten@telia.com>
2016-08-30 12:23:45 +0200

    initial commit

---
+++ askfullname/askfullname/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace askfullname
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "";
+            Console.WriteLine("what is your first name?");
+            name += Console.ReadLine();
+            Console.WriteLine("what is your last name?");
+            name += " " + Console.ReadLine();
+            Console.WriteLine("hello "+name+"!");
+            Console.ReadLine();
+        }
+    }
+}
