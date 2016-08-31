0867b2040015177d0375f3513ce7661fd9a9e3d3
Linus Alpsten <linus.alpsten@telia.com>
2016-08-31 10:00:37 +0200

    simple calculator using windows forms

---
+++ winforms_calculator/winforms_calculator/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace winforms_calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "Enter first number";
+            label2.Text = "Enter second number";
+            button1.Text = "+";
+            button2.Text = "-";
+            button3.Text = "*";
+            button4.Text = "/";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show((int.Parse(textBox1.Text) + int.Parse(textBox2.Text)).ToString(),"Answer", MessageBoxButtons.OKCancel);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show((int.Parse(textBox1.Text) - int.Parse(textBox2.Text)).ToString(), "Answer", MessageBoxButtons.OKCancel);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show((int.Parse(textBox1.Text) * int.Parse(textBox2.Text)).ToString(), "Answer", MessageBoxButtons.OKCancel);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show((int.Parse(textBox1.Text) / int.Parse(textBox2.Text)).ToString(), "Answer", MessageBoxButtons.OKCancel);
+        }
+    }
+}
