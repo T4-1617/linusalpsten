4a75d17674557f78c6ad484cf00673b9492fcbe8
Linus Alpsten <linus.alpsten@telia.com>
2016-09-12 12:38:12 +0200

    [Uppgift0912] created main functions

---
+++ Uppgift0912 Customer Application/Uppgift0912 Customer Application/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Uppgift0912_Customer_Application
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbLastName = new System.Windows.Forms.TextBox();
+            this.tbFirstName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.customerlist = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbLastName
+            // 
+            this.tbLastName.Location = new System.Drawing.Point(132, 80);
+            this.tbLastName.Name = "tbLastName";
+            this.tbLastName.Size = new System.Drawing.Size(100, 20);
+            this.tbLastName.TabIndex = 1;
+            // 
+            // tbFirstName
+            // 
+            this.tbFirstName.Location = new System.Drawing.Point(132, 44);
+            this.tbFirstName.Name = "tbFirstName";
+            this.tbFirstName.Size = new System.Drawing.Size(100, 20);
+            this.tbFirstName.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Förnamn";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Efternamn";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(204, 116);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(28, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // customerlist
+            // 
+            this.customerlist.FormattingEnabled = true;
+            this.customerlist.Location = new System.Drawing.Point(50, 157);
+            this.customerlist.Name = "customerlist";
+            this.customerlist.Size = new System.Drawing.Size(182, 82);
+            this.customerlist.TabIndex = 5;
+            this.customerlist.SelectedIndexChanged += new System.EventHandler(this.customerlist_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Mina kunder";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(47, 242);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.customerlist);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbFirstName);
+            this.Controls.Add(this.tbLastName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbLastName;
+        private System.Windows.Forms.TextBox tbFirstName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox customerlist;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Uppgift0912 Customer Application/Uppgift0912 Customer Application/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uppgift0912_Customer_Application
+{
+    public partial class Form1 : Form
+    {
+        public class Customer
+        {
+            public string firstName { get; set; }
+            public string lastName { get; set; }
+            public int ID { get; set; }
+
+            public string fullname()
+            {
+                return string.Format("{0} {1}", firstName, lastName);
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            label4.Text = "Du har 0st kunder";
+        }
+
+        public List<Customer> customers = new List<Customer>();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (customers.Count >= 100)
+            {
+                MessageBox.Show("Du har för många kunder");
+                return;
+            }
+            Customer c = new Customer();
+            c.firstName = tbFirstName.Text;
+            c.lastName = tbLastName.Text;
+            c.ID = 100 + customers.Count;
+
+            customers.Add(c);
+
+            customerlist.Items.Clear();
+            foreach (Customer name in customers)
+            {
+                customerlist.Items.Add(name.fullname());
+            }
+            label4.Text = string.Format("Du har {0}st kunder", customers.Count);
+        }
+
+        private void customerlist_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Customer c = customers[customerlist.SelectedIndex];
+            MessageBox.Show(string.Format("ID: {0}\nFörnamn: {1}\nEfternamn: {2}\nAktiv: True", c.ID, c.firstName, c.lastName));
+        }
+    }
+}
